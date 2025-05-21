7a150632b28d5218e8f68cb525e8ddea91051097
dirque <160206667+dirque@users.noreply.github.com>
2025-05-21 07:35:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post("/scrape", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Scraper server running on port ${PORT}`);
 });
